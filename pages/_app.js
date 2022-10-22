a6dea6ce50a1257f1dc78b4a8905a0fd03f5b431
Gabriel Okunola <arowosegbe67@gmail.com>
2022-10-22 21:39:33 +0100

    seo and pwa features

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,16 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div id='app'>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta
+          name='description'
+          content='Choice9ja Goal is to provide a platform for Nigerian citizens, that’s aimed to increase transparency, strengthened participatory  governance and ensuring accountability of our leaders, wherein its online community can raise issues, and rate politicians according to their actions. '
+        />
+        <title>The Choice 9ja</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Provider store={store}>
-        <Head>
-          <title>The Choice 9ja</title>
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
         <GlobalStoreProvider>
           <Component {...pageProps} />
         </GlobalStoreProvider>
